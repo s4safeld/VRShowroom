23a8f233c81a3e171af9ed686b791abf6a8d67ca
s4safeld <57436321+s4safeld@users.noreply.github.com>
2020-12-09 19:20:33 +0100

    Added an InputManager which will be able to detect all Controller inputs. Began work on a basic controller selection functionality, this will be the basis for the remote selection and collection feature.

---
+++ Assets/InputDebugger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class InputDebugger : MonoBehaviour {
+    private InputManager inputManager;
+    // Start is called before the first frame update
+    void Start() {
+        inputManager = GetComponent<InputManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (inputManager.triggerButtonPressed(XRNode.LeftHand)) {
+            Debug.Log("left Trigger pressed");
+        }
+
+        if (inputManager.thumbrestPressed(XRNode.LeftHand)) {
+            Debug.Log("left Thumb Rest pressed");
+        }
+
+        if (inputManager.gripValue(XRNode.LeftHand) != 0.0f) {
+            Debug.Log("left gripped pressed with value: " + inputManager.gripValue(XRNode.LeftHand));
+        }
+
+        if (inputManager.triggerButtonPressed(XRNode.RightHand)) {
+            Debug.Log("Right Trigger pressed");
+        }
+
+        if (inputManager.thumbrestPressed(XRNode.RightHand)) {
+            Debug.Log("Right Thumb Rest pressed");
+        }
+
+        if (inputManager.gripValue(XRNode.RightHand) != 0.0f) {
+            Debug.Log("Right gripped pressed with value: " + inputManager.gripValue(XRNode.RightHand));
+        }
+        */
+    }
+}
---
+++ Assets/InputManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.XR;
+
+public class InputManager : MonoBehaviour {
+    private XRNode leftHand = XRNode.LeftHand;
+    private XRNode rightHand = XRNode.RightHand;
+    
+    private List<InputDevice> devicesLeft = new List<InputDevice>();
+    private List<InputDevice> devicesRight = new List<InputDevice>();
+
+    public InputDevice deviceLeft;
+    public InputDevice deviceRight;
+
+    private void GetLeftDevice() {
+        InputDevices.GetDevicesAtXRNode(leftHand, devicesLeft);
+        deviceLeft = devicesLeft.FirstOrDefault();
+
+    }
+    private void GetRightDevice() {
+        InputDevices.GetDevicesAtXRNode(rightHand, devicesRight);
+        deviceRight = devicesRight.FirstOrDefault();
+    }
+    
+
+    private void OnEnable() {
+        if (!deviceLeft.isValid) {
+            GetLeftDevice();
+        }
+        if (!deviceRight.isValid) {
+            GetRightDevice();
+        }
+    }
+
+    private void Update() {
+        if (!deviceLeft.isValid) {
+            GetLeftDevice();
+        }
+
+        if (!deviceRight.isValid) {
+            GetRightDevice();
+        }
+    }
+
+    public bool triggerButtonPressed(XRNode controller) {
+        if (controller == XRNode.LeftHand) {
+            return (deviceLeft.TryGetFeatureValue(CommonUsages.triggerButton, out bool value) && value);
+        }
+        if (controller == XRNode.RightHand) {
+            return (deviceRight.TryGetFeatureValue(CommonUsages.triggerButton, out bool value) && value);   
+        }
+        Debug.LogError("Something went wrong in InputManager.triggerButtonPressed(), controller seems to be neither right nor left");
+        return false;
+    }
+
+    public float gripValue(XRNode controller) {
+        switch (controller) {
+            case XRNode.LeftHand: {
+                return deviceLeft.TryGetFeatureValue(CommonUsages.grip, out float value) ? value : 0.0f;
+            }
+            case XRNode.RightHand: {
+                return deviceRight.TryGetFeatureValue(CommonUsages.grip, out float value) ? value : 0.0f;
+            }
+            default:
+                Debug.LogError(
+                    "Something went wrong in InputManager.gripPressed(), controller seems to be neither right nor left");
+                return 0.0f;
+        }
+    }
+
+    public bool thumbrestPressed(XRNode controller) {
+        if (controller == XRNode.LeftHand) {
+            return (deviceLeft.TryGetFeatureValue(CommonUsages.thumbrest, out bool value) && value);
+        }
+
+        if (controller == XRNode.RightHand) {
+            return (deviceRight.TryGetFeatureValue(CommonUsages.thumbrest, out bool value) && value);
+        }
+
+        Debug.LogError(
+            "Something went wrong in InputManager.thumbrestPressed(), controller seems to be neither right nor left");
+        return false;
+    }
+
+}
