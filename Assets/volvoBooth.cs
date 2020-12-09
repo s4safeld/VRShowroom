76a67af3f909a49145d8e231237033eb929e62f8
s4safeld <57436321+s4safeld@users.noreply.github.com>
2020-12-09 15:53:44 +0100

    Imported some more models and added a Hand model into the scene. Additionally some basic rotation for the car was added just so something is moving.

---
+++ Assets/volvoBooth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volvoBooth : MonoBehaviour
+{
+    public bool rotate = true;
+    public float rotationSpeed = 1f;
+
+    void Update() {
+        transform.Rotate(new Vector3(0f, rotationSpeed, 0f) * Time.deltaTime);
+    }
+}
