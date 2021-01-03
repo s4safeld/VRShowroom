cd36d0f0d1cf2707b65ce5da577effa4f15fc4d5
s4safeld <57436321+s4safeld@users.noreply.github.com>
2021-01-03 16:01:49 +0100

    Decided to completely redo the project, the new one is based on an example scene which should make development way easier.
    While there is a nice looking locomotion system now,
    and a very good remote system on the rise.
    
    The lighting is off and the performance shit, fixing needed.

---
+++ Assets/Remote.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Remote : MonoBehaviour {
+    public bool grabbed = false;
+    private MyInputManager mim;
+    public Transform currHand;
+    public Vector3 holdingPosition;
+
+    void Start() {
+        mim = FindObjectOfType<MyInputManager>();
+    }
+
+    void Update() {
+
+        if (grabbed) {
+            Debug.Log("grabbed");
+        }
+
+        if (!grabbed && mim.GripValue('l') > 0) {
+            if (Vector3.Distance(transform.position, mim.leftHandController.position) < 1) {
+                grabbed = true;
+                transform.Rotate(holdingPosition);
+                currHand = mim.leftHandController;
+            }
+        }
+
+        if (!grabbed && mim.GripValue('r') > 0) {
+            if (Vector3.Distance(transform.position, mim.rightHandController.position) < 1) {
+                grabbed = true;
+                transform.Rotate(holdingPosition);
+                currHand = mim.rightHandController;
+            }
+        }
+
+        if (grabbed) {
+            if (currHand == mim.rightHandController) {
+                if (!mim.TriggerButtonPressed('r'))
+                    grabbed = false;
+            }
+            else {
+                if (!mim.TriggerButtonPressed('l'))
+                    grabbed = false;
+            }
+        }
+    }
+}
